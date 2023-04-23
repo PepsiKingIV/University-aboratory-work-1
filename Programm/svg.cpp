a96c81a105e85038ec66094ca1e40dbc558d1c3f
Фонов Александр <FonovAD@Mpei.ru>
2023-04-23 19:14:53 +0300

    Дабавлен вывод гистограмм в формате svg

--- Programm/svg.cpp
+++ Programm/svg.cpp
@@ -21,9 +21,40 @@ void svg_text(double left, double baseline, std::string text)
     std::cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
 }
 
+void svg_rect(double x, double y, double width, double height, std::string stroke = "black", std::string fill = "black")
+{
+    std::cout << "<rect x='" << x << "' y='" << y << "' width='" << width 
+              << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << " '/>";
+}
+
 void show_histogram_svg(const std::vector<size_t> &bins)
 {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+
     svg_begin(400, 300);
-    svg_text(20, 20, std::to_string(bins[0]));
+    double top = 0;
+
+    size_t max_count = 0;
+    for (auto bin : bins)
+    {
+        if (bin > max_count)
+            max_count = bin;
+    }
+    const auto BLOCK_WIDTH = (IMAGE_WIDTH - TEXT_WIDTH) / max_count;
+
+    for (size_t bin : bins)
+    {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, std::to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "blue", "#aaffaa");
+        top += BIN_HEIGHT;
+    }
+    // svg_text(20, 20, std::to_string(bins[0]));
+    // svg_rect(50, 0, bins[0] * 10, 30);
     svg_end();
 }
